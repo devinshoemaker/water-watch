05429191b215b110c4ac57032fd84f50ac768e74
Devin Shoemaker <devinshoe@gmail.com>
2019-10-23 06:20:20 -0500

    firebase: set persistence to none

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,5 +5,6 @@ import { environment } from './environments/environment';
 
 firebase.initializeApp(environment.firebase);
 export const auth = firebase.auth();
+auth.setPersistence(firebase.auth.Auth.Persistence.NONE);
 
 export default firebase;
